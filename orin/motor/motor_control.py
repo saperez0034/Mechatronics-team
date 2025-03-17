ff986143cdef78bb268a4f5e3b52bcb87de21abb
SebaPV <52762939+SebaPV@users.noreply.github.com>
2025-03-16 22:09:34 -0400

    combining latest state machine code with this branch

---
+++ orin/motor/motor_control.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import serial
+import time
+
+
+def send_data(ser, str):
+    for ch in str:
+        ser.write(ch.encode("utf-8"))
+        time.sleep(0.01)
+
+
+# if __name__ == "__main__":
+def motor_setup():
+    ser = serial.Serial("/dev/ttyACM0", 115200)
+
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
+    ser.flush()
+    return ser
+
+
+if __name__ == "__main__":
+    ser = motor_setup()
+    cmd = 'toggleled\r'
+    while (1):
+        send_data(cmd, ser)
+        time.sleep(1)
