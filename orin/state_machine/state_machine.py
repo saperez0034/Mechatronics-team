f6c22df92d67804ca69cc997dcb54bb53830ee4a
Sebastian Perez-Villarrubia <sebavilla0034@gmail.com>
2025-03-24 17:53:38 -0400

    initial PID logic implemented, added some more state machine logic. To be tested.

--- orin/state_machine/state_machine.py
+++ orin/state_machine/state_machine.py
@@ -15,9 +15,6 @@ class StateMachine:
         self.current_state = 'INITIAL'
         self.pipeline = None
         self.ser = None
-        self.h_range = None
-        self.s_range = None
-        self.v_range = None
         self.needle_x = 320
         self.needle_y = 240
         self.lesion_midpoint = None
@@ -31,14 +28,23 @@ class StateMachine:
         self.dt = 0.1
         self.stepx = 0
         self.stepy = 0
-        self.lesions = None
-        self.lesion_index = 0
         self.led_command = "toggleled\r"
         self.lin_act_extend_command = "lin_act 1\r"
         self.lin_act_retract_command = "lin_act 0\r"
+        self.lin_stepper_extend_command = "lin_stepper 100\r"
+        self.lin_stepper_retract_command = "lin_stepper 0\r"
         self.stepperY_command = "stepY 10\r"
         self.stepperX_command = "stepX 10\r"
         self.stepperX_midpoint_steps = 100
+
+    def move_x (self, steps):
+        stm32.send_data(self.ser, 'stepx '+ str(steps) + '\r')
+
+    def move_y (self, steps):
+        stm32.send_data(self.ser, 'stepy '+ str(steps) + '\r')
+
+    def release_sample(self):
+        stm32.send_data(self.ser, self.lin_act_extend_command)
 
     def initial_state(self):
         print("Initial state")
@@ -47,14 +53,9 @@ class StateMachine:
         self.ser = stm32.stm32_setup()
         for i in range(3):
             stm32.send_data(self.ser, "\r")
+        stm32.send_data(self.ser, self.lin_stepper_retract_command)
         stm32.send_data(self.ser, self.lin_act_extend_command)
         self.current_state = 'DETECTING'
-
-    def move_x (self, steps):
-        stm32.send_data(self.ser, 'stepx '+ str(steps) + '\r')
-
-    def move_y (self, steps):
-        stm32.send_data(self.ser, 'stepy '+ str(steps) + '\r')
 
     def detecting_state(self):
         print("Detecting state")
@@ -65,42 +66,42 @@ class StateMachine:
             stm32.send_data(self.ser, self.stepperY_command)
             self.current_state = 'DETECTING'
         else:
-            self.lesions = result
-            # self.h_range, self.s_range, self.v_range, self.lesion_midpoint = result
-            # # print("Detected Grape Color Ranges (HSV):")
-            # # print(f"Hue: {self.h_range[0]:.2f} - {self.h_range[1]:.2f}")
-            # # print(f"Saturation: {self.s_range[0]:.2f} - {self.s_range[1]:.2f}")
-            # # print(f"Value: {self.v_range[0]:.2f} - {self.v_range[1]:.2f}")
-            # pid.pid_controller(self.needle_x, self.lesion_midpoint[0], self.kp, self.ki, self.kd, self.pid_error_x, )
+            self.lesion_midpoint = result
             self.current_state = 'PROCESSING'
 
     def processing_state(self):
         print("Processing state")
-        self.lesion_midpoint = self.lesions[self.lesion_index][3]
-        self.step_x, self.pid_error_x, self.i_x = pid.pid_controller(self.needle_x, self.lesion_midpoint[0], self.kp, self.ki, self.kd, self.pid_error_x, self.i_x, self.dt)
-        self.step_y, self.pid_error_x, self.i_x = pid.pid_controller(self.needle_y, self.lesion_midpoint[0], self.kp, self.ki, self.kd, self.pid_error_y, self.i_y, self.dt)
-        self.move_x(self, self.step_x)
-        self.move_y(self, self.step_y)
-        self.lesion_index = self.lesion_index + 1
-        if (self.lesion_index == self.lesions.len()):
-            self.current_state = 'FINAL'
+        if (self.pid_error_x > 0.1):
+            self.step_x, self.pid_error_x, self.i_x = pid.pid_controller(
+                        self.needle_x, self.lesion_midpoint[0], self.kp, self.ki, 
+                        self.kd, self.pid_error_x, self.i_x, self.dt)
+            self.move_x(self, self.step_x)
+        if (self.pid_error_y > 0.1):
+            self.step_y, self.pid_error_x, self.i_x = pid.pid_controller(
+                        self.needle_y, self.lesion_midpoint[0], self.kp, self.ki, 
+                        self.kd, self.pid_error_y, self.i_y, self.dt)
+            self.move_y(self, self.step_y)
+        if (self.pid_error_x < 0.1 and self.pid_error_y < 0.1):
+            self.current_state = 'EXTRACT_SAMPLE'
+        self.current_state = 'DETECTING'
 
     def extract_sample_state(self):
         print("Extracting sample state")
         # Add extraction logic here
         # Transition to the next state
+        stm32.send_data(self.ser, self.lin_stepper_extend_command)
+        time.sleep(10)
         stm32.send_data(self.ser, self.lin_act_retract_command)
-
-    def release_sample(self):
-        print("Releasing sample state")
-        # Add release logic here
-        # Transition to the next state
-        stm32.send_data(self.ser, self.lin_act_extend_command)
+        time.sleep(1)
+        stm32.send_data(self.ser, self.lin_stepper_retract_command)
+        time.sleep(10)
+        self.current_state = 'FINAL'
 
     def final_state(self):
         print("Final state")
         # Add finalization logic here
         # Transition to the next state or end
+        self.release_sample()
         stm32.send_data(self.ser, self.led_command)
         self.current_state = 'DETECTING'
     
