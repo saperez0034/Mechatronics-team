925b7d463c5c87cb08dfc56aaf91381789842399
Sebastian Perez-Villarrubia <sebavilla0034@gmail.com>
2025-04-15 14:52:43 -0400

    Fixed limit switch code

--- stm32/cli_and_motors/Core/Src/stepper.c
+++ stm32/cli_and_motors/Core/Src/stepper.c
@@ -6,6 +6,7 @@ void vStepperControlX (void){
     int32_t steps;
     for (;;)
     {
+    	int8_t prev_dir = 0;
         xTaskNotifyWait(pdFALSE,    // Don't clear bits on entry
             0,  // Clear all bits on exit
             &steps, // Receives the notification value
@@ -14,12 +15,29 @@ void vStepperControlX (void){
         {
             HAL_GPIO_WritePin(stp1_dir_GPIO_Port, stp1_dir_Pin, GPIO_PIN_RESET);
             steps = steps * -1;
+            prev_dir = 0;
         }
-        else
+        else {
             HAL_GPIO_WritePin(stp1_dir_GPIO_Port, stp1_dir_Pin, GPIO_PIN_SET);
-        
+        	prev_dir = 1;
+        }
+
         for (uint32_t pwm = 0; pwm < steps; pwm++){
             if (stepper_x_stop){
+                osDelay(100);
+                if (prev_dir) {
+					HAL_GPIO_WritePin(stp1_dir_GPIO_Port, stp1_dir_Pin, GPIO_PIN_RESET);
+                }
+                else {
+                	HAL_GPIO_WritePin(stp1_dir_GPIO_Port, stp1_dir_Pin, GPIO_PIN_SET);
+                }
+                osDelay(50);
+                for (uint32_t i = 0; i < 50; i++) {
+                	HAL_GPIO_WritePin(stp1_pul_GPIO_Port, stp1_pul_Pin, GPIO_PIN_SET);
+					osDelay(1);
+					HAL_GPIO_WritePin(stp1_pul_GPIO_Port, stp1_pul_Pin, GPIO_PIN_RESET);
+					osDelay(1);
+                }
                 stepper_x_stop = 0;
                 break;
             }
@@ -35,6 +53,7 @@ void vStepperControlY (void){
     int32_t steps;
     for (;;)
     {
+    	int8_t prev_dir = 0;
         xTaskNotifyWait(pdFALSE,    // Don't clear bits on entry
             0,  // Clear all bits on exit
             &steps, // Receives the notification value
@@ -43,12 +62,29 @@ void vStepperControlY (void){
         {
             HAL_GPIO_WritePin(stp2_dir_GPIO_Port, stp2_dir_Pin, GPIO_PIN_RESET);
             steps = steps * -1;
+            prev_dir = 0;
         }
-        else
+        else {
             HAL_GPIO_WritePin(stp2_dir_GPIO_Port, stp2_dir_Pin, GPIO_PIN_SET);
-        
+            prev_dir = 1;
+        }
+
         for (uint32_t pwm = 0; pwm < steps; pwm++){
             if (stepper_y_stop){
+                osDelay(100);
+                if (prev_dir) {
+                	HAL_GPIO_WritePin(stp2_dir_GPIO_Port, stp2_dir_Pin, GPIO_PIN_RESET);
+				}
+				else {
+					HAL_GPIO_WritePin(stp2_dir_GPIO_Port, stp2_dir_Pin, GPIO_PIN_SET);
+				}
+                osDelay(50);
+                for (uint32_t i = 0; i < 50; i++) {
+                	HAL_GPIO_WritePin(stp2_pul_GPIO_Port, stp2_pul_Pin, GPIO_PIN_SET);
+					osDelay(1);
+					HAL_GPIO_WritePin(stp2_pul_GPIO_Port, stp2_pul_Pin, GPIO_PIN_RESET);
+					osDelay(1);
+				}
                 stepper_y_stop = 0;
                 break;
             }
@@ -57,5 +93,6 @@ void vStepperControlY (void){
             HAL_GPIO_WritePin(stp2_pul_GPIO_Port, stp2_pul_Pin, GPIO_PIN_RESET);
             osDelay(1);
         }
+
     }
 }
