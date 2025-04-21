004a4243c4aaa5ef3e8dfd0f5c12c25a5e7b0b1b
Sebastian Perez <saperez@andrew.cmu.edu>
2025-04-21 15:47:02 -0400

    Perrin Cooked some Wagyu

--- orin/state_machine/state_machine.py
+++ orin/state_machine/state_machine.py
@@ -164,7 +164,7 @@ class StateMachine:
 
     def extract_sample_state(self):
         print("Extracting sample state")
-        self.lin_servo(35)
+        self.lin_servo(38)
         time.sleep(1.5)
         self.lin_act(1)
         time.sleep(1)
