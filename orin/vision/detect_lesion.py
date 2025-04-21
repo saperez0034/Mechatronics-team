a15cc90027246cb2d226ab7f9faea0b3d65f6cdb
perrintong <perrintong@cmu.edu>
2025-04-21 13:13:08 -0400

    enable depth

--- orin/vision/detect_lesion.py
+++ orin/vision/detect_lesion.py
@@ -164,6 +164,7 @@ def vision_setup():
         exit(0)
 
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
     # Start streaming
     pipeline.start(config)
